ea67a8ea4bcd703bf18a1c261d09a368d195afab
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 01:11:36 -0400

    Pedido.h

--- proyecto_final/include/Pedido.h
+++ proyecto_final/include/Pedido.h
@@ -6,21 +6,24 @@
 #include "Platillo.h"
 #include "Bebida.h"
 #include "Global.h"
-// #include "Cliente.h" // <-- LÍNEA ELIMINADA
 
-class Cliente; // <-- LÍNEA AÑADIDA
+class Cliente;
 
 class Pedido {
 public:
     int idPedido;
-    Cliente* cliente; // <-- Ahora es válido
+    Cliente* cliente; 
     std::vector<Platillo> listaPlatillos;
     std::vector<Bebida> listaBebidas;
     EstadoPedido estado;
+//Se crea un objeto tipo time_point el cual guarda el momento exactoq ue se creo el pedido esto con fines de
+// calcular tiempos de preparacion de platillos o bebidas
     std::chrono::system_clock::time_point tiempoCreacion;
-
+//COnstructor de Pedido
     Pedido(int id, Cliente* c, const std::vector<Platillo>& p, const std::vector<Bebida>& b);
+//Calcula total del pedido
     double calcularTotal() const;
+//Actualizacion del estado del pedido a un nuevo valor
     void actualizarEstado(EstadoPedido nuevoEstado);
 };
 
