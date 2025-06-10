012796ef03fe21da7ad6472d6ecdebf258895256
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 03:35:41 -0400

    Pedido.cpp

--- proyecto_final/scr/Pedido.cpp
+++ proyecto_final/scr/Pedido.cpp
@@ -1,10 +1,11 @@
 #include "Pedido.h"
 
+//Constructor de Pedido
 Pedido::Pedido(int id, Cliente* c, const std::vector<Platillo>& p, const std::vector<Bebida>& b)
     : idPedido(id), cliente(c), listaPlatillos(p), listaBebidas(b), estado(EstadoPedido::Pendiente) {
     tiempoCreacion = std::chrono::system_clock::now();
 }
-
+//Metodo que calcula el total sumando los precios del platillo y de la bebida
 double Pedido::calcularTotal() const {
     double total = 0.0;
     for (const auto& platillo : listaPlatillos) {
@@ -13,9 +14,10 @@ double Pedido::calcularTotal() const {
     for (const auto& bebida : listaBebidas) {
         total += bebida.obtenerPrecio();
     }
+    //retorno del total de la cuenta
     return total;
 }
-
+//Cambia de estado el Pedido
 void Pedido::actualizarEstado(EstadoPedido nuevoEstado) {
     estado = nuevoEstado;
 }
