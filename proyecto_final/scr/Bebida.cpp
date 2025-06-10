43824020f0ce8f6930c1f3d3575040d9197ebdba
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 03:06:03 -0400

    Bebida.cpp

--- proyecto_final/scr/Bebida.cpp
+++ proyecto_final/scr/Bebida.cpp
@@ -2,6 +2,7 @@
 
 Bebida::Bebida(std::string n, int t, double p) : nombre(n), tiempoPreparacion(t), precio(p) {}
 
+//Retorna el precio de la Bebida
 double Bebida::obtenerPrecio() const {
     return precio;
 }
