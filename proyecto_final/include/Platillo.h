3f2bef9703931ce91d9b7a6c5b56bc0d7b8014ca
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 01:17:37 -0400

    Platillo.h

--- proyecto_final/include/Platillo.h
+++ proyecto_final/include/Platillo.h
@@ -9,8 +9,9 @@ public:
     std::string nombre;
     int tiempoPreparacion;
     double precio;
+//Secuencia que se debe de seguir para la preparacion de un platillo 
     std::vector<std::string> secuenciaElectrodomesticos;
-
+//Constructor de Platillo
     Platillo(std::string n, int t, double p);
     double obtenerPrecio() const;
     int obtenerTiempoPreparacion() const;
