f934a5645de41d235293d095efd61cda6b9ecaa4
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 03:21:19 -0400

    Platillo.cpp

--- proyecto_final/scr/Platillo.cpp
+++ proyecto_final/scr/Platillo.cpp
@@ -2,10 +2,11 @@
 
 Platillo::Platillo(std::string n, int t, double p) : nombre(n), tiempoPreparacion(t), precio(p) {}
 
+//Obtiene el precio del platillo
 double Platillo::obtenerPrecio() const {
     return precio;
 }
-
+//Obtiene el tiempo de preparacion del platillo
 int Platillo::obtenerTiempoPreparacion() const {
     return tiempoPreparacion;
 }
