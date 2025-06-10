25b6990b3b6605d6a955778b9135cfbcc4d19b42
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 01:35:59 -0400

    Trabajador.h

--- proyecto_final/include/Trabajador.h
+++ proyecto_final/include/Trabajador.h
@@ -2,14 +2,16 @@
 #define TRABAJADOR_H
 
 #include <string>
-
+//Declaracion de la clase Padre Trabajador
 class Trabajador {
 public:
     int idTrabajador;
     std::string nombre;
     std::string rol;
-
+//Constructor
     Trabajador(int id, std::string n, std::string r);
+//Destructor virtual, se hace virtual para que las clases derivadas puedan sobreescribirlo por temas de
+//dinamica
     virtual ~Trabajador() = default;
 
     // Método virtual puro que las clases hijas deben implementar.
