353ef8d1b6c314bf9e19f9f83c392e44ecddf55c
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 03:02:27 -0400

    Electrodomestico.cpp

--- proyecto_final/scr/Electrodomestico.cpp
+++ proyecto_final/scr/Electrodomestico.cpp
@@ -2,10 +2,11 @@
 
 Electrodomestico::Electrodomestico(std::string t) : tipo(t) {}
 
+//Bloqueo o pone en estado ocupado el Electrodomestrico
 void Electrodomestico::usar() {
     mtx.lock();
 }
-
+//Libera el Electrodomestico
 void Electrodomestico::liberar() {
     mtx.unlock();
 }
