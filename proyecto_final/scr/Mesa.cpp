fe426c135227eaf602dca9a467e28124e6a6af8c
Chocomilk7714 <alejandrotambo3@gmail.com>
2025-06-10 03:04:18 -0400

    Mesa.cpp

--- proyecto_final/scr/Mesa.cpp
+++ proyecto_final/scr/Mesa.cpp
@@ -2,14 +2,15 @@
 
 Mesa::Mesa(int num, int cap) : numeroMesa(num), capacidad(cap), ocupada(false) {}
 
+//Comprueba si la mesa esta disponible o no
 bool Mesa::estaDisponible() const {
     return !ocupada;
 }
-
+// Ocupa la mesa
 void Mesa::ocupar() {
     ocupada = true;
 }
-
+//Libera la mesa
 void Mesa::liberar() {
     ocupada = false;
 }
